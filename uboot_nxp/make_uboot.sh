592b5c3dd61ab19043e111a0f4222144f74ec22e
zhangchao <zc9286@live.cn>
2021-03-20 06:07:27 -0700

    Add: nxp origin kernel and uboot

---
+++ uboot_nxp/make_uboot.sh
@@ -0,0 +1,16 @@
+source /opt/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/environment
+#make mx6ull_14x14_ddr512_emmc_defconfig
+make mx6ull_14x14_evk_emmc_defconfig
+make all -j
+mv u-boot.imx u-boot-imx6ull-14x14-ddr512-emmc.imx
+mv u-boot.bin u-boot-imx6ull-14x14-ddr512-emmc.bin
+
+if [ ! -e "./tmp" ]; then
+    mkdir tmp
+fi
+rm -rf tmp/*
+
+mv u-boot-imx6ull*.bin tmp
+mv u-boot-imx6ull*.imx tmp
+
+echo "tmp/u-boot-imx6ull-14x14-ddr512-emmc.imx is ready "
