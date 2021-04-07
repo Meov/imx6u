21ea8508ca58641f9cdd6359646c4106d9539f08
zhangchao <zc9286@live.cn>
2021-04-07 06:20:49 -0700

    update: remove all compiled files add: mx6ull_test_emmc issue add: tftp nfs directories

--- uboot_nxp/make_uboot.sh
+++ uboot_nxp/make_uboot.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 source /opt/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/environment
-#make mx6ull_14x14_ddr512_emmc_defconfig
-make mx6ull_14x14_evk_emmc_defconfig
+
+make mx6ull_test_emmc_defconfig
+#make mx6ull_14x14_evk_emmc_defconfig
 make all -j
-mv u-boot.imx u-boot-imx6ull-14x14-ddr512-emmc.imx
-mv u-boot.bin u-boot-imx6ull-14x14-ddr512-emmc.bin
+mv u-boot.imx u-boot-imx6ull-ddr512-emmc.imx
+mv u-boot.bin u-boot-imx6ull-ddr512-emmc.bin
 
 if [ ! -e "./tmp" ]; then
     mkdir tmp
@@ -13,4 +16,4 @@ rm -rf tmp/*
 mv u-boot-imx6ull*.bin tmp
 mv u-boot-imx6ull*.imx tmp
 
-echo "tmp/u-boot-imx6ull-14x14-ddr512-emmc.imx is ready "
+echo "tmp/u-boot.bin  is ready "
